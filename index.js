ab5d24a5f1c75c0d2714651cc0c61c00b999b295
Annika Klüpfel <AnnikaKluepfel@web.de>
2020-10-28 20:46:49 +0100

    updated everything to connect to Helder's DB

--- index.js
+++ index.js
@@ -14,3 +14,9 @@ app.get('/test', function(req, res) {
 app.listen(port, function() {
     console.log(`Heroku app listening at https://annikasheroku.herokuapp.com/`);
   });
+
+
+  //nmp i dotenv: enables you to create invis. .env file in root where you can store connection and pw information
+  //and the info will be applied to main code in proper syntax.
+  //of course the .env file itself shouldn't be shared or pushed to gitHub > pw, username etc. remain with you
+  //after installing npm package it can be quickly applied by saying reqire.'dotenv'.config()
