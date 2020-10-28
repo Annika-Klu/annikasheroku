a57155862380bec2b5d0874411b1293c155d4a6e
Annika Klüpfel <AnnikaKluepfel@web.de>
2020-10-28 20:49:49 +0100

    copied Helder's code to my index file :D

--- index.js
+++ index.js
@@ -1,19 +1,88 @@
-const express = require('express');
+require('dotenv').config()
 
-const app = express();
-const port = process.env.PORT || 3000;
+const express = require('express')
+const mysql = require('mysql2')
 
-//request handlers
+const app = express()
+const port = process.env.PORT
 
-app.get('/test', function(req, res) {
-   res.json("Hi!");
-});
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+})
 
-//listener
+app.get('/', function(req, res) {
+
+    console.log(`A requeste was madde to the home endpoint!`)
+
+    res.json({
+        status: 'ok',
+        description: `it's ok what else can I say...`
+    })
+})
+
+app.get('/dbtest', function(req, res) {
+
+    connection.query(`SELECT * FROM offices`, function(error, result) {
+    
+        res.json(result)
+    
+    })
+})
+
+app.get('/api/employees', function(req, res) {
+
+    connection.query(`SELECT * FROM employees`, function(error, result) {
+    
+        res.json(result)
+    
+    })
+})
+
+app.get('/api/employees/:id', function(req, res) {
+
+    const id = req.params.id
+
+    connection.query(`SELECT * FROM employees WHERE employeeNumber = ?`, [id], function(error, result) {
+        
+        if (error) {
+            res.json({
+                status: 'error',
+                description: `it's an error, try again later`
+            })
+        }
+
+        res.json(result)
+    
+    })
+})
 
 app.listen(port, function() {
-    console.log(`Heroku app listening at https://annikasheroku.herokuapp.com/`);
-  });
+    console.log(`running on port ${port}`)
+})
+
+
+//first setup when making project run hosted by heroku
+//----------------------------------------------------
+
+// const express = require('express');
+
+// const app = express();
+// const port = process.env.PORT || 3000;
+
+// //request handlers
+
+// app.get('/test', function(req, res) {
+//    res.json("Hi!");
+// });
+
+// //listener
+
+// app.listen(port, function() {
+//     console.log(`Heroku app listening at https://annikasheroku.herokuapp.com/`);
+//   });
 
 
   //nmp i dotenv: enables you to create invis. .env file in root where you can store connection and pw information
