1e6674628381fd49882fdf4f069610dd64117341
Annika Klüpfel <AnnikaKluepfel@web.de>
2020-11-09 16:23:45 +0100

    added semicolon to line 7

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const mysql = require('mysql2');
 
 const app = express();
-const port = process.env.PORT
+const port = process.env.PORT;
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
